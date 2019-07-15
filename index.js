1c3d3d2b0127d1120b334305864d8af9cc7972b0
Michael DiCarlo <michael.v.dicarlo@gmail.com>
2019-07-15 11:53:45 +0000

    Change to more lightweight server

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const { send, json } = require('micro')
+const { router, get, post } = require('microrouter');
 const logger = require('winston');
-const rateLimit = require('express-rate-limit');
 
 const {
     transports,
     format
-} = require('winston');
+} = logger;
 
 const {
     combine,
@@ -20,51 +19,17 @@ logger.configure({
     ),
     transports: [
         new transports.Console({
-            level: 'error',
-        }),
-        new transports.Console({
             level: 'info',
         }),
     ],
 });
 
-const app = express();
-app.enable('trust proxy');
-const limiter = rateLimit({
-    windowMs: 5 * 60 * 1000,
-    max: 20,
-    message: 'Too many requests from this IP'
-});
+const pong = (req, res) => send(res, 200, 'pong');
 
-app.use(limiter);
-app.use(bodyParser.json({limit: '150kb'}));
-app.use(bodyParser.urlencoded({
-    extended: false,
-    limit: '0kb',
-}));
+const log = async (req, res) => {
+  const body = await json(req);
+  logger.info(JSON.stringify(body.errorLog, null, 1));
+  send(res, 200, 'Log accepted');
+}
 
-
-app.get('/', (req, res) => {
-    res.send('pong!');
-});
-
-app.post('/log/error', (req, res) => {
-  if (req.body.errorLog) {
-    try {
-      const log = req.body.errorLog;
-      logger.error(JSON.stringify(log, null, 1));
-      res.status(200).send('Log accepted');
-    } catch (err) {
-      logger.log('error', '/log/error exception', err);
-      res.status(500).send(err);
-    }
-  } else {
-    res.sendStatus(500);
-  }
-});
-
-app.listen(3000, () => {
-  logger.info('Server listening on port 3000');
-});
-
-module.exports = app;
+module.exports = router(post('/log/error', log), get('/*', pong));
